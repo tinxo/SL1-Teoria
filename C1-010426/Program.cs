7b924cfc5d069bf7600784072a3bbe90556c68e1
Martin Rey <m.rey00@gmail.com>
2026-04-01 15:56:29 -0300

    Contenido de la clase 1 2026 del 01/04/2026.

---
+++ C1-010426/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hola SL1 cosecha 2026!");
+int numero = 10;
+// Console.WriteLine("El numero es: " + numero);
+int numero2 = 20;
+
+int suma = numero + numero2;
+Console.WriteLine($"La suma de {numero} y {numero2} es: {suma}");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Iteración {i}");
+}
+
+Console.Write("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Hola, {nombre}!");
+
