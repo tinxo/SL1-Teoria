214d74c92efa3c871ae8668b0b79dd6e34efab5f
Martin Rey <m.rey00@gmail.com>
2026-04-16 09:34:19 -0300

    Cambios de la clase del 15/04. Paso a funciones de la lógica de control de peso de equipaje.

--- C2-consola/Program.cs
+++ C2-consola/Program.cs
@@ -1,4 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int obtenerCantidadEquipaje(string tipoTarifa)
+{
+    int cantidadItems;
+    if (tipoTarifa.ToUpper() == "P" || tipoTarifa.ToUpper() == "B")
+    {
+        cantidadItems = 1;
+    }
+    else if (tipoTarifa.ToUpper() == "F")
+    {
+        cantidadItems = 2;
+    }
+    else
+    {
+        cantidadItems = 3;
+    }
+    return cantidadItems;
+}
+
+bool verificarPesoEquipaje(double pesoEquipaje, int nroItem, string tipoVuelo)
+{
+    double pesoLimiteNacional = 0.0;
+    double pesoLimiteInternacional = 0.0;
+    if (nroItem == 1)
+    {
+        // solo el primer item tiene estas restricciones de peso
+        pesoLimiteNacional = 8.0;
+        pesoLimiteInternacional = 10.0;
+    } else
+    {
+        // item despachado en bodega, tiene estas restricciones de peso
+        pesoLimiteNacional = 15.0;
+        pesoLimiteInternacional = 23.0;
+    }
+    if ((tipoVuelo.ToUpper() == "N") && (pesoEquipaje > pesoLimiteNacional))
+    {
+        return false;
+    }
+    else if ((tipoVuelo.ToUpper() == "I") && (pesoEquipaje > pesoLimiteInternacional))
+    {
+       return false;
+    }
+    else
+    {
+        return true; // el peso del equipaje es válido para el tipo de vuelo
+    }
+
+}
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Sistema de Check-in para Aerolineas Argentinas");
 Console.WriteLine("Ingrese su nombre:");
 string nombre = Console.ReadLine();
@@ -25,51 +74,25 @@ Console.WriteLine($"Tipo de Tarifa: {tipoTarifa}");
 Console.WriteLine($"Número de Vuelo: {nroVuelo}");
 
 // Pesaje de equipaje
-int cantidadItems;
-if (tipoTarifa.ToUpper() == "P" || tipoTarifa.ToUpper() == "B")
-{
-    cantidadItems = 1;
-} else if (tipoTarifa.ToUpper() == "F")
-{
-    cantidadItems = 2;
-} else
-{
-    cantidadItems = 3;
-}
+// 1ro. Se establece la cantidad de items de equipaje que puede despachar
+int cantidadItems = obtenerCantidadEquipaje(tipoTarifa);
+
 Console.WriteLine($"Cantidad de items de equipaje a despachar: {cantidadItems}");
-double pesoLimiteNacional = 0.0;
-double pesoLimiteInternacional = 0.0;
+// 2da. Se solicita y valida el peso de cada item
+bool resultado;
 for (int i = 1; i <= cantidadItems; i++)
 {
     Console.WriteLine($"Ingrese el peso total del equipaje (kg) para el item {i}:");
     double pesoEquipaje = Convert.ToDouble(Console.ReadLine());
     Console.WriteLine($"Peso del equipaje para el item {i}: {pesoEquipaje} kg");
-    if (i == 1)
+    resultado = verificarPesoEquipaje(pesoEquipaje, i, tipoVuelo);
+    if (!resultado)
     {
-        // solo el primer item tiene estas restricciones de peso
-        pesoLimiteNacional = 8.0;
-        pesoLimiteInternacional = 10.0;
-    } else
-    {
-        // item despachado en bodega, tiene estas restricciones de peso
-        pesoLimiteNacional = 15.0;
-        pesoLimiteInternacional = 23.0;
+        Console.WriteLine($"El equipaje del item {i} excede el peso permitido para el tipo de vuelo.");
     }
-    if (tipoVuelo.ToUpper() == "N")
+    else
     {
-        if (pesoEquipaje > pesoLimiteNacional)
-        {
-            Console.WriteLine($"El equipaje del item {i} excede el peso permitido para vuelos nacionales.");
-        }
-    }
-    else if (tipoVuelo.ToUpper() == "I")
-    {
-        if (pesoEquipaje > pesoLimiteInternacional)
-        {
-            Console.WriteLine($"El equipaje del item {i} excede el peso permitido para vuelos internacionales.");
-        }
+        Console.WriteLine($"El equipaje del item {i} cumple con el peso permitido para el tipo de vuelo.");
     }
 }
-
-
 Console.WriteLine("-----------------------------");
